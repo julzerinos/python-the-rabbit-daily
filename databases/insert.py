6675892b565a75e9f17c58d4799412f45099203a
julzerinos <julzerinos@gmail.com>
2019-08-31 13:17:43 +0200

    Add email regex check and insert helper function

--- databases/insert.py
+++ databases/insert.py
@@ -1,7 +1,12 @@
+import sys
+
 from db_conn_decr import dbconnect
 from db_actions import subs_insert_subs, subs_view_subs
 
 
+if len(sys.argv) < 2:
+    raise SystemExit
+
 with dbconnect('databases/subscribers.db') as conn:
-    subs_insert_subs(conn, ['nibre0809@gmail.com',])
+    subs_insert_subs(conn, sys.argv[1:])
     print(subs_view_subs(conn))
